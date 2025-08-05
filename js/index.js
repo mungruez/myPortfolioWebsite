c1f04d38753ae59cea6ce5eb6d936a34fd6f282f
Zaakir Mungrue <mungruez@gmail.com>
2025-08-05 06:49:25 -0400

    Update index.js
    js for the buttun

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+    btn.addEventListener('mousemove', (e) => {
+        let x = e.pageX - btn.offsetLeft;
+        let y = e.pageY - btn.offsetTop;
+    
+        btn.style.setProperty('--x', x + 'px');
+        btn.style.setProperty('--y', y + 'px');
+    });
+})
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -9,4 +20,4 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-})
+});
